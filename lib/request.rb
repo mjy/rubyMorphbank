04e07a3337e3deac68dd8f52e241c509b02aeba2
mjy <diapriid@gmail.com>
2010-05-12 15:17:01 -0400

    initial load

---
+++ lib/request.rb
@@ -0,0 +1,53 @@
+class Request
+
+  # defaults
+  METHOD = 'search' 
+  OBJECTTYPE = 'Image'
+  LIMIT = 10 
+  DEPTH = 1
+  FORMAT = 'id'
+
+  # a subset of VALID_OPTIONS, these are concatenated in the request
+  URI_PARAMS = [:limit, :format, :depth, :objecttype, :first_result, :keywords, :id]
+
+  # possible options for a Request instance 
+  VALID_OPTIONS = {'v3' => URI_PARAMS + [:version, :method] } 
+
+  attr_reader(:request_url, :request_options)
+
+  def initialize(options = {}) 
+    opt = {
+      :version => 'v3',
+      :method => METHOD,
+      :limit => LIMIT,
+      :format => FORMAT,                # 'id' (brief results) or 'svc' (schema based results)
+      :depth => DEPTH,
+      :objecttype => OBJECTTYPE,        #  [nil, Taxon, Image, Character, Specimen, View, Matrix, Locality, Collection, OTU ] (nil = all)
+      :first_result => 0,
+      :keywords => ''
+    }.merge!(options)
+
+    # check for legal parameters
+    opt.keys.each do |p|
+      raise RubyMorphbankError, "#{p} is not a valid parameter" if !VALID_OPTIONS[opt[:version]].include?(p)
+    end 
+
+
+    # create the request 
+    @request_url = SERVICES_URI + "method=#{opt[:method]}" +
+    opt.keys.sort{|a,b| a.to_s <=> b.to_s}.collect{
+      |k| ( (URI_PARAMS.include?(k) && (!opt[k].nil? || opt[k].empty?)) ? "&#{k.to_s}=#{opt[k]}" : '')
+    }.join
+
+    # and some housekeepers
+    @request_options = opt 
+  end
+
+  def get_response
+    Response.new(self)
+  end
+
+  # .root.elements.each(xpath){}.map do |row|
+
+    ## REXML::XPath.match(@xml, '//object/width').collect{|e| e.text.to_s}
+end
---
+++ lib/response.rb
@@ -0,0 +1,74 @@
+class Response
+  
+  attr_reader(
+              :response,              # the Net:HTTP response
+              :doc,                   # the REXML:Document for :response
+              :request                # cache the request object for reference in the response
+             )
+
+  # all the objects returned
+  attr :objects, true
+  attr :ids, true
+
+  attr :annotations
+  attr :images
+
+  def initialize(request = Request.new)
+
+   
+    begin 
+      @response = Net::HTTP.get_response(URI.parse(request.request_url)).body
+    rescue SocketError
+      raise "can not connect to socket, check SERVICES_URI"
+    end 
+
+    # TODO: check that we're getting a legit XML document back before trying to parse it
+    @doc = REXML::Document.new(@response)
+
+    @request = request # TODO: can this be aliased some how?
+    self
+  end
+
+  # return an Array of ids representing morphbankIds to images
+  def mb_image_ids
+    if request.request_options[:format] == 'id' 
+      REXML::XPath.match(@doc, "//id").collect{|e| e.text.to_s}
+    else
+      raise RubyMorphbankError, "you must use method='image' in requests to use the mb_image_ids method"
+    end
+  end
+
+  # get the Integer of the first match
+  def get_int(element_name)
+    REXML::XPath.first(@doc, "//#{element_name}").nil? ? 0 : REXML::XPath.first(@doc, "//#{element_name}").text.to_i
+  end
+
+  # get the String of the first match
+  def get_text(element_name)
+    REXML::XPath.first(@doc, "//#{element_name}").nil? ? 0 : REXML::XPath.first(@doc, "//#{element_name}").text
+  end
+
+  # return an Array of XML elements whose root is an object element
+  def objects
+    REXML::XPath.match(@doc, "//object") 
+  end
+
+  # TODO: the has conversion is NOT optimal (no attributes, children of root only), see extension in rubyMorpbank
+  def hashified_objects
+    self.doc.records("//object")
+  end
+
+  # pagination
+  def link_forward?
+    ((get_int('numResultsReturned') + get_int('firstResult')) < get_int('numResults')) and return true
+    false
+  end
+
+  # pagination
+  def link_back?
+    (get_int('firstResult') > @request.request_options[:limit]) and return true
+    false
+  end
+
+end
+
